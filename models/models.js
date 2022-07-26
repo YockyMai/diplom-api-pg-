a1c581fe3eb94207250e9ff75555f4b93bfa6eb0
Grigoriev Valerii <g.valera15102003@gmail.com>
2022-07-26 13:31:30 +0500

    fix

--- models/models.js
+++ models/models.js
@@ -1,5 +1,5 @@
 const sequelize = require('../db');
-const { DataTypes } = require('Sequelize');
+const { DataTypes } = require('sequelize');
 
 const User = sequelize.define('user', {
 	id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
