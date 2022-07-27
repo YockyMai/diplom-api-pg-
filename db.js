b49269a850aed4a20253baba45a1b4022d305f3f
Grigoriev Valerii <g.valera15102003@gmail.com>
2022-07-27 16:15:45 +0500

    отключил проверку ssl соединения

--- db.js
+++ db.js
@@ -1,16 +1,11 @@
 const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
-	{
-		connectionString: process.env.DB_HOST,
-		dialect: 'postgres',
-		dialectOptions: {
-			ssl: {
-				rejectUnauthorized: false,
-			},
-		},
+module.exports = new Sequelize(process.env.DB_URL, {
+	dialect: 'postgres',
+	dialectOptions: {
+		require: true,
+		rejectUnauthorized: false,
+		ssl: true,
+		native: true,
 	},
-);
+});
